5a775280d8fe747f5011e9ea7a958fa4c4690eb8
truxish <justforgithubidk@gmail.com>
2024-05-15 08:56:12 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-export default function Home () {
+export default function Home() {
   return (
     <>
+      <div>
+        <h1 className="h1-bold">We're coming for you!</h1>
+      </div>
     </>
-  )
+  );
 }
