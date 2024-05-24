d22b0f84d5c316b53f64b910b0f1118986fc1b21
truxish <justforgithubidk@gmail.com>
2024-05-24 18:03:14 +0530

    added assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,21 +6,49 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Coldflow",
+  description:
+    "A platform where you can ask questions about your medical conditions to the doctors themselves for certainly free.",
+
+  icons: {
+    icon: "/coldflow/app/favicon.ico",
+  },
+};
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  preload: false,
+  variable: "--font-inter",
+});
+
+export const space_grotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  preload: false,
+  variable: "--font-space_grotesk",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <body className={`${inter.variable} ${space_grotesk.variable}`}>
           {children}
         </body>
       </html>
